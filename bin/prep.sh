f7d23a42720aa65fec21a3ef57f87805242ccf00
Leon Valencia <techie@lynx.rocks>
2021-09-08 15:19:14 +0800

    fixed issue with using expanded storage in dev env

--- bin/prep.sh
+++ bin/prep.sh
@@ -5,15 +5,16 @@ yarn global add --force aws-cdk
 
 # Grow storage
 # ToDo: Use CF output or SSM on dev-env and pick values up here.
-# ToDo: Add Secrets Manager credentials creation for Magento marketplace here. Put credentials on cdk.context.yaml once it's git ignored.
+# ToDo: Ask for admin details and credentials and store in Secrets Manager. Read name from cdk.context.yaml. No need to .gitignore cdk.context.yaml.
+# ToDo: Ask for Magento marketplace credentials and store in Secrets Manager. Read name from cdk.context.yaml.
 C9_NAME=C9forSS
 CF_NAME=$(aws cloudformation list-stacks --stack-status-filter CREATE_COMPLETE | jq -r --arg C9_NAME ${C9_NAME} '.StackSummaries[] | select(.StackName| contains($C9_NAME)) | .StackName')
 EC2_ID=$(aws cloudformation list-stack-resources --stack-name ${CF_NAME} | jq -r '.StackResourceSummaries[] | select(.LogicalResourceId == "Instance") | .PhysicalResourceId')
 VOL_ID=$(aws ec2 describe-instances --instance-ids ${EC2_ID} | jq -r '.Reservations[0].Instances[0].BlockDeviceMappings[0].Ebs.VolumeId')
 aws ec2 modify-volume --volume-id ${VOL_ID} --size 50
-until [ $(aws ec2 describe-volumes-modifications --volume-id ${VOL_ID} | jq '.VolumesModifications[0].ModificationState') != "modifying" ]; do : ; done
 
 # Use storage
+until [ $(aws ec2 describe-volumes-modifications --volume-id ${VOL_ID} | jq -r '.VolumesModifications[0].ModificationState') == "optimizing" ]; do : ; done
 DEV="/dev/nvme0n1"
 PART=${DEV}"p1"
 FS=$(df -hT | grep ${PART} | awk '{ print $2 }')
